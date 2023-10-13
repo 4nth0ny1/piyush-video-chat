380f61b1efe486c0b5aadc3a289860f025b0b02f
4nth0ny1 <acatullo4@gmail.com>
2023-10-12 20:28:34 -0500

    now multiple users can join the same room

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
+import { useSocket } from "../context/SocketProvider";
 
 const RoomPage = () => {
+  const socket = useSocket();
+
+  const handleUserJoined = useCallback(({ email, id }) => {
+    console.log(`Email ${email} joined room`);
+  }, []);
+
+  useEffect(() => {
+    socket.on("user:joined", handleUserJoined);
+
+    return () => {
+      socket.off("user:joined", handleUserJoined);
+    };
+  }, [socket, handleUserJoined]);
+
   return (
     <div>
       <h2>Room Page</h2>
